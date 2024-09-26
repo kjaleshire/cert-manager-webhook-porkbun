0b0cd5beb4a75cfe9de097c9ab21839609ed49a7
Kyle J Aleshire <kjaleshire@gmail.com>
2024-09-26 09:12:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Porkbun Webhook solver for cert-manager
+
+This is an unofficial webhook solver for [cert-manager](https://cert-manager.io/) and [Porkbun](https://porkbun.com/).
+
+## Usage
+
+1. Deploy the webhook:
+
+    ```
+    helm install porkbun-webhook ./deploy/porkbun-webhook \
+        --set groupName=<your group>
+    ```
+
+2. Create a secret containing your [API key](https://porkbun.com/account/api):
+
+    ```
+    kubectl create secret generic porkbun-key \
+        --from-literal=api-key=<your key> \
+        --from-literal=secret-key=<your key>
+    ```
+
+3. Create a role and role binding:
+
+    ```
+    kubectl apply -f rbac.yaml
+    ```
+
+4. Configure a certificate issuer:
+
+    ```yaml
+    apiVersion: cert-manager.io/v1
+    kind: ClusterIssuer
+    metadata:
+      name: letsencrypt
+    spec:
+      acme:
+        server: https://acme-v02.api.letsencrypt.org/directory
+        email: <your e-mail>
+        privateKeySecretRef:
+          name: letsencrypt-key
+        solvers:
+          - selector:
+              dnsZones:
+                - <your domain>
+            dns01:
+              webhook:
+                groupName: <your group>
+                solverName: porkbun
+                config:
+                  apiKeySecretRef:
+                    name: porkbun-key
+                    key: api-key
+                  secretKeySecretRef:
+                    name: porkbun-key
+                    key: secret-key
+    ```
+
+## Running the test suite
+
+All DNS providers **must** run the DNS01 provider conformance testing suite,
+else they will have undetermined behaviour when used with cert-manager.
+
+**It is essential that you configure and run the test suite when creating a
+DNS01 webhook.**
+
+To run the tests, first put your api key into testdata/porkbun-solver/porkbun-secret.yaml.
+
+Then you can run the test suite with:
+
+```bash
+TEST_ZONE_NAME=<your domain>. make test
+```
---
+++ porkbun/porkbun.go
@@ -0,0 +1,160 @@
+package porkbun
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/cert-manager/cert-manager/pkg/acme/webhook"
+	acme "github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
+	"github.com/nrdcg/porkbun"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	klog "k8s.io/klog/v2"
+)
+
+type PorkbunSolver struct {
+	kube *kubernetes.Clientset
+}
+
+func (e *PorkbunSolver) Name() string {
+	return "porkbun"
+}
+
+type Config struct {
+	ApiKeySecretRef    corev1.SecretKeySelector `json:"apiKeySecretRef"`
+	SecretKeySecretRef corev1.SecretKeySelector `json:"secretKeySecretRef"`
+}
+
+func (e *PorkbunSolver) readConfig(request *acme.ChallengeRequest) (*porkbun.Client, error) {
+	config := Config{}
+
+	if request.Config != nil {
+		if err := json.Unmarshal(request.Config.Raw, &config); err != nil {
+			return nil, fmt.Errorf("config error: %s", err)
+		}
+	}
+
+	apiKey, err := e.resolveSecretRef(config.ApiKeySecretRef, request)
+	if err != nil {
+		return nil, err
+	}
+
+	secretKey, err := e.resolveSecretRef(config.SecretKeySecretRef, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return porkbun.New(secretKey, apiKey), nil
+}
+
+func (e *PorkbunSolver) resolveSecretRef(selector corev1.SecretKeySelector, ch *acme.ChallengeRequest) (string, error) {
+	secret, err := e.kube.CoreV1().Secrets(ch.ResourceNamespace).Get(context.Background(), selector.Name, metav1.GetOptions{})
+	if err != nil {
+		return "", fmt.Errorf("get error for secret %q %q: %s", ch.ResourceNamespace, selector.Name, err)
+	}
+
+	bytes, ok := secret.Data[selector.Key]
+	if !ok {
+		return "", fmt.Errorf("secret %q %q does not contain key %q: %s", ch.ResourceNamespace, selector.Name, selector.Key, err)
+	}
+
+	return string(bytes), nil
+}
+
+func (e *PorkbunSolver) Present(ch *acme.ChallengeRequest) error {
+	klog.Infof("Handling present request for %q %q, zone %q", ch.ResolvedFQDN, ch.Key, ch.ResolvedZone)
+
+	client, err := e.readConfig(ch)
+	if err != nil {
+		return fmt.Errorf("initialization error: %s", err)
+	}
+
+	domain := strings.TrimSuffix(ch.ResolvedZone, ".")
+	entity := strings.TrimSuffix(ch.ResolvedFQDN, "."+ch.ResolvedZone)
+	name := strings.TrimSuffix(ch.ResolvedFQDN, ".")
+
+	klog.Infof("Retrieving records for domain %q", domain)
+	records, err := client.RetrieveRecords(context.Background(), domain)
+	if err != nil {
+		return fmt.Errorf("retrieve records error: %s", err)
+	}
+
+	for _, record := range records {
+		if record.Type == "TXT" && record.Name == name && record.Content == ch.Key {
+			klog.Infof("Record %s is already present", record.ID)
+			return nil
+		}
+	}
+
+	id, err := client.CreateRecord(context.Background(), domain, porkbun.Record{
+		Name:    entity,
+		Type:    "TXT",
+		Content: ch.Key,
+		TTL:     "60",
+	})
+	if err != nil {
+		return fmt.Errorf("create record error: %s", err)
+	}
+
+	klog.Infof("Created record %v", id)
+	return nil
+}
+
+func (e *PorkbunSolver) CleanUp(ch *acme.ChallengeRequest) error {
+	klog.Infof("Handling cleanup request for %q %q", ch.ResolvedFQDN, ch.Key)
+
+	client, err := e.readConfig(ch)
+	if err != nil {
+		return fmt.Errorf("initialization error: %s", err)
+	}
+
+	domain := strings.TrimSuffix(ch.ResolvedZone, ".")
+	name := strings.TrimSuffix(ch.ResolvedFQDN, ".")
+	records, err := client.RetrieveRecords(context.Background(), domain)
+	if err != nil {
+		return fmt.Errorf("retrieve records error: %s", err)
+	}
+
+	for _, record := range records {
+		if record.Type == "TXT" && record.Name == name && record.Content == ch.Key {
+			id, err := strconv.ParseInt(record.ID, 10, 32)
+			if err != nil {
+				return fmt.Errorf("found TXT record, but it's ID is malformed: %s", err)
+			}
+
+			record.Content = ch.Key
+			err = client.DeleteRecord(context.Background(), domain, int(id))
+			if err != nil {
+				return fmt.Errorf("delete record error: %s", err)
+			}
+
+			klog.Infof("Deleted record %v", id)
+			return nil
+		}
+	}
+
+	klog.Info("No matching record to delete")
+
+	return nil
+}
+
+func (e *PorkbunSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
+	klog.Info("Initializing")
+
+	kube, err := kubernetes.NewForConfig(kubeClientConfig)
+	if err != nil {
+		return fmt.Errorf("kube client creation error: %s", err)
+	}
+
+	e.kube = kube
+	return nil
+}
+
+func New() webhook.Solver {
+	return &PorkbunSolver{}
+}
